cc03046e52cac187578b7b95ec393fcc067d1fec
Maxchenn <max2468tw@gmail.com>
2018-05-08 20:19:35 +0800

    test for EMBARC_ROOT = ../embARC

---
+++ my_gpio/obj_emsk_22/gnu_arcem7d/emsk/emsk_timer.o.d
@@ -0,0 +1,28 @@
+obj_emsk_22/gnu_arcem7d/emsk/emsk_timer.o: \
+ ../embARC/board/emsk/common/emsk_timer.c ../embARC/inc/arc/arc_builtin.h \
+ ../embARC/inc/embARC_toolchain.h ../embARC/inc/arc/arc.h \
+ ../embARC/inc/arc/arc_timer.h ../embARC/inc/arc/arc.h \
+ ../embARC/inc/arc/arc_exception.h ../embARC/inc/arc/arc_builtin.h \
+ ../embARC/board/board.h ../embARC/board/emsk/emsk.h \
+ ../embARC/inc/arc/arc_em.h ../embARC/board/emsk/drivers/iic/dw_iic_obj.h \
+ ../embARC/device/device_hal/inc/dev_iic.h \
+ ../embARC/device/device_hal/inc/dev_common.h \
+ ../embARC/board/emsk/drivers/spi/dw_spi_obj.h \
+ ../embARC/device/device_hal/inc/dev_spi.h \
+ ../embARC/board/emsk/drivers/uart/dw_uart_obj.h \
+ ../embARC/device/device_hal/inc/dev_uart.h \
+ ../embARC/board/emsk/drivers/gpio/dw_gpio_obj.h \
+ ../embARC/device/designware/gpio/dw_gpio.h \
+ ../embARC/device/device_hal/inc/dev_gpio.h \
+ ../embARC/board/emsk/drivers/gpio/emsk_gpio.h \
+ ../embARC/board/emsk/drivers/spiflash/spiflash.h \
+ ../embARC/inc/embARC_error.h \
+ ../embARC/board/emsk/drivers/pmwifi/pmwifi.h \
+ ../embARC/device/microchip/mrf24g/wf_dev_hal.h \
+ ../embARC/device/device_hal/inc/dev_wnic.h \
+ ../embARC/board/emsk/drivers/temperature/emsk_temperature.h \
+ ../embARC/board/emsk/drivers/sdcard/emsk_sdcard.h \
+ ../embARC/board/emsk/drivers/ntshell/ntshell_io.h \
+ ../embARC/board/emsk/drivers/ble/hm1x.h \
+ ../embARC/board/emsk/common/emsk_timer.h \
+ ../embARC/board/emsk/configs/22/emsk_hardware.h
---
+++ my_kernel/obj_emsk_22/gnu_arcem7d/emsk/dw_iic_obj.o.d
@@ -0,0 +1,29 @@
+obj_emsk_22/gnu_arcem7d/emsk/dw_iic_obj.o: \
+ ../embARC/board/emsk/drivers/iic/dw_iic_obj.c ../embARC/inc/arc/arc.h \
+ ../embARC/inc/arc/arc_builtin.h ../embARC/inc/embARC_toolchain.h \
+ ../embARC/inc/embARC_error.h ../embARC/inc/arc/arc_exception.h \
+ ../embARC/inc/arc/arc.h ../embARC/inc/arc/arc_builtin.h \
+ ../embARC/device/designware/iic/dw_iic.h \
+ ../embARC/device/device_hal/inc/dev_iic.h \
+ ../embARC/device/device_hal/inc/dev_common.h \
+ ../embARC/board/emsk/drivers/iic/dw_iic_obj.h \
+ ../embARC/board/emsk/drivers/iic/../../emsk.h ../embARC/inc/arc/arc_em.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/iic/dw_iic_obj.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/spi/dw_spi_obj.h \
+ ../embARC/device/device_hal/inc/dev_spi.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/uart/dw_uart_obj.h \
+ ../embARC/device/device_hal/inc/dev_uart.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/gpio/dw_gpio_obj.h \
+ ../embARC/device/designware/gpio/dw_gpio.h \
+ ../embARC/device/device_hal/inc/dev_gpio.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/gpio/emsk_gpio.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/spiflash/spiflash.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/pmwifi/pmwifi.h \
+ ../embARC/device/microchip/mrf24g/wf_dev_hal.h \
+ ../embARC/device/device_hal/inc/dev_wnic.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/temperature/emsk_temperature.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/sdcard/emsk_sdcard.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/ntshell/ntshell_io.h \
+ ../embARC/board/emsk/drivers/iic/../../drivers/ble/hm1x.h \
+ ../embARC/board/emsk/drivers/iic/../../common/emsk_timer.h \
+ ../embARC/board/emsk/configs/22/emsk_hardware.h
