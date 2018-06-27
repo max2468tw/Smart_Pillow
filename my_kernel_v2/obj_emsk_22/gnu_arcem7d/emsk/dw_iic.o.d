2bd3c9112f9ba30ad112f06bc2b28fe4d366ece1
Maxchenn <max2468tw@gmail.com>
2018-06-27 14:00:37 +0800

    update my_kernel_v2

---
+++ my_kernel_v2/obj_emsk_22/gnu_arcem7d/emsk/dw_iic.o.d
@@ -0,0 +1,10 @@
+obj_emsk_22/gnu_arcem7d/emsk/dw_iic.o: \
+ ../embARC/device/designware/iic/dw_iic.c \
+ ../embARC/inc/embARC_toolchain.h ../embARC/inc/embARC_error.h \
+ ../embARC/inc/arc/arc_builtin.h ../embARC/inc/arc/arc_exception.h \
+ ../embARC/inc/arc/arc.h ../embARC/inc/arc/arc_builtin.h \
+ ../embARC/device/designware/iic/dw_iic_hal.h \
+ ../embARC/device/designware/iic/dw_iic_hal_cfg.h \
+ ../embARC/device/designware/iic/dw_iic.h \
+ ../embARC/device/device_hal/inc/dev_iic.h \
+ ../embARC/device/device_hal/inc/dev_common.h
---
+++ my_kernel_v2/obj_emsk_22/gnu_arcem7d/middleware/common/xprintf.o.d
@@ -0,0 +1,3 @@
+obj_emsk_22/gnu_arcem7d/middleware/common/xprintf.o: \
+ ../embARC/middleware/common/xprintf/xprintf.c \
+ ../embARC/middleware/common/xprintf/xprintf.h
